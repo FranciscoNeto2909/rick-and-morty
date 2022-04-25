72e794ada8e35ebc2b1a6b25d03782e7ea1a9ddc
Francisco Neto <wantedshark@gmail.com>
2022-04-25 09:31:37 -0300

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const charactersUrl = "https://rickandmortyapi.com/api/character"
+const episodesUrl = "https://rickandmortyapi.com/api/episode"
+
+export function getCharacters(){
+    
+}
+export function getEpisodes(){
+
+}
